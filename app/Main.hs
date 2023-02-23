9cd623c251c05aca45be33d86ebf9996b3e286f3
osawa-koki <github@osawa.dev>
2023-02-23 13:33:44 +0900

    小さな修正。

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,10 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    let x :: Integer
+        x = 10
+        y :: Integer
+        y = 20
+        result = x + y
+    putStrLn (show x ++ " + " ++ show y ++ " = " ++ show result)
